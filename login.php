a5f08fbef453e4b18da8705813968433b0f022c4
S-Syah <139469238+S-Syah@users.noreply.github.com>
2023-10-14 12:45:51 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,11 +1,1 @@
-<?php
 
-$valid_password = "your_password";
-$input_username = $_POST['username'];
-$input_password = $_POST['password'];
-if ($input_username === $valid_username && $input_password === $valid_password) {
-    echo "Login successful. Welcome, $valid_username!";
-} else {
-    echo "Login failed. Please check your username and password.";
-}
-?>
